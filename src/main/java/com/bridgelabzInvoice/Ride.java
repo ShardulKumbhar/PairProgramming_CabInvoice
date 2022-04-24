9f1aa9af53b65cee92f9231c4ab5d890629c6901
Shardulkumbhar <kumbharshardul384@gmail.com>
2022-04-25 01:53:37 +0530

    Added : UC2 Logic For Multiple Rides

---
+++ src/main/java/com/bridgelabzInvoice/Ride.java
@@ -0,0 +1,14 @@
+package com.bridgelabzInvoice;
+
+public class Ride {
+    public double distance;
+    public int time;
+
+    /*
+    created Constructor
+     */
+    public Ride(double distance , int time ){
+        this.distance=distance;
+        this.time=time;
+    }
+}
